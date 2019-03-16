fe51945abac5360767dcc900baa5bb68df4dde87
Rajasegar <rajasegarc@freshdesk.com>
2019-03-16 06:52:51 +0530

    Initial commmit with astexplorer assets

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # ember-angle-brackets-codemod
-Codemod to convert curly braces syntax to angle brackets syntax
+A jscodeshift Codemod to convert curly braces syntax to angle brackets syntax
+
+## Usage
+
+### 1. Install jscodeshift
+Get jscodeshift from npm:
+
+```sh
+$ npm install -g jscodeshift
+```
+This will install the runner as jscodeshift.
+
+### 2. Run the transform
+```sh
+jscodeshift -t https://raw.githubusercontent.com/rajasegar/ember-angle-brackets-codemods/master/transform.js app/templates
+```
+
+
+## From
+```hbs
+{{site-header user=this.user class=(if this.user.isAdmin "admin")}}
+
+{{#super-select selected=this.user.country as |s|}}
+  {{#each this.availableCountries as |country|}}
+    {{#s.option value=country}}{{country.name}}{{/s.option}}
+  {{/each}}
+{{/super-select}}
+```
+
+## To
+```hbs
+<SiteHeader @user={{this.user}} class={{if this.user.isAdmin "admin"}} />
+
+<SuperSelect @selected={{this.user.country}} as |Option|>
+  {{#each this.availableCountries as |country|}}
+    <Option @value={{country}}>{{country.name}}</Option>
+  {{/each}}
+</SuperSelect>
+```
+
+## AST Explorer playground
+[AST Explorer](https://astexplorer.net/#/gist/b128d5545d7ccc52400b922f3b5010b4/571266d8c29cb8eb1bd5730c0c388526081cce46)
+
+## Known issues
+- No formatting preserved
+- Block params need to be tweaked
---
+++ transform.js
@@ -0,0 +1,86 @@
+const HTML_ATTRIBUTES = [
+  "class",
+  "value",
+  "title",
+  "label",
+  "placeholder",
+  "required",
+  
+
+];
+
+const ignoreBlocks = ["each"];
+
+const capitalizedTagName = tagname =>
+  tagname
+    .split("-")
+    .map(s => {
+      return s[0].toUpperCase() + s.slice(1);
+    })
+    .join("");
+
+module.exports = function({ plugins, syntax }) {
+  const { parse, print, builders, traverse, Walker } = syntax;
+  const b = builders;
+
+  const transformAttrs = attrs => {
+    return attrs.map(a => {
+      let _key = a.key;
+      let _valueType = a.value.type;
+      let _value;
+      if (!HTML_ATTRIBUTES.includes(a.key)) {
+        _key = "@" + _key;
+      }
+
+      if (_valueType === "PathExpression") {
+        _value = b.mustache(b.path(a.value.original));
+      } else if (_valueType === "SubExpression") {
+        const params = a.value.params.map(p => p.original).join(" ");
+        _value = b.mustache(b.path(a.value.path.original + " " + params));
+      } else {
+        _value = b.text(a.value.original);
+      }
+
+      return b.attr(_key, _value);
+    });
+  };
+
+
+
+  return {
+    name: "ast-transform",
+
+    visitor: {
+      ElementNode(node) {},
+
+      MustacheStatement(node) {
+        // Don't change attribute statements
+    if (node.loc.source !== "(synthetic)" && node.hash.pairs.length > 0) {
+      const tagname = node.path.original;
+      const _capitalizedTagName = capitalizedTagName(tagname);
+      const attributes = transformAttrs(node.hash.pairs);
+
+      return b.element(_capitalizedTagName, { attrs: attributes });
+    }
+      },
+      BlockStatement(node) {
+        if (!ignoreBlocks.includes(node.path.original)) {
+
+          
+          console.log(node);
+
+    const tagname = node.path.original;
+    let _capitalizedTagName = capitalizedTagName(tagname);
+    let attributes = transformAttrs(node.hash.pairs);
+
+    return b.element(_capitalizedTagName, {
+      attrs: attributes,
+      children: node.program.body,
+
+      blockParams: node.program.blockParams
+    });
+        }
+      }
+    }
+  };
+};
