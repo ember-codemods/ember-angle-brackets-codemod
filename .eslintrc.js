a30da69dc1ff317c20c10cb610be110881b71133
Ryan Mark <rgmark@gmail.com>
2019-07-05 21:15:05 -0700

    Add eslint (#93)

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  },
+  overrides: [{
+    files: ['**/*.test.js'],
+    env: {
+      jest: true
+    }
+  }]
+};
--- transforms/angle-brackets/transforms/angle-brackets-syntax.js
+++ transforms/angle-brackets/transforms/angle-brackets-syntax.js
@@ -1,7 +1,7 @@
-const glimmer = require("@glimmer/syntax");
-const prettier = require("prettier");
-const path = require("path");
-const fs = require("fs");
+const glimmer = require('@glimmer/syntax');
+const prettier = require('prettier');
+const path = require('path');
+const fs = require('fs');
 
 const _EMPTY_STRING_ = `ANGLE_BRACKET_EMPTY_${Date.now()}`;
 
@@ -30,130 +30,130 @@ class Config {
 /**
  * List of HTML attributes for which @ should not be appended
  */
-const HTML_ATTRIBUTES = ["class", "placeholder", "required"];
+const HTML_ATTRIBUTES = ['class', 'placeholder', 'required'];
 
-const BUILT_IN_COMPONENTS = ["link-to", "input", "textarea"];
+const BUILT_IN_COMPONENTS = ['link-to', 'input', 'textarea'];
 
 /**
  * Ignore the following list of MustacheStatements from transform
  * Politely lifted from https://github.com/lifeart/ember-ast-hot-load/blob/master/lib/ast-transform.js#L26
  */
 const IGNORE_MUSTACHE_STATEMENTS = [
-  "identity", // glimmer blocks
-  "render-inverse", // glimmer blocks
-  "-get-dynamic-var", // glimmer internal helper
-  "-lf-get-outlet-state", // dunno
-  "action",
-  "component",
-  "hot-content",
-  "hot-placeholder",
-  "if",
-  "if-unless",
-  "each",
-  "each-in",
-  "format-date",
-  "format-message",
-  "format-relative",
-  "format-time",
-  "format-money",
-  "format-number",
-  "unless",
-  "in-element",
-  "query-params",
-  "-in-element",
-  "-class",
-  "-html-safe",
-  "-input-type",
-  "-normalize-class",
-  "concat",
-  "get",
-  "mut",
-  "readonly",
-  "unbound",
-  "debugger",
-  "else",
-  "let",
-  "with",
-  "log",
-  "loc",
-  "hash",
-  "partial",
-  "yield",
-  "t",
-  "t-for",
-  "transition-to",
-  "get-meta",
-  "get-attr",
-  "index-of",
-  "array",
+  'identity', // glimmer blocks
+  'render-inverse', // glimmer blocks
+  '-get-dynamic-var', // glimmer internal helper
+  '-lf-get-outlet-state', // dunno
+  'action',
+  'component',
+  'hot-content',
+  'hot-placeholder',
+  'if',
+  'if-unless',
+  'each',
+  'each-in',
+  'format-date',
+  'format-message',
+  'format-relative',
+  'format-time',
+  'format-money',
+  'format-number',
+  'unless',
+  'in-element',
+  'query-params',
+  '-in-element',
+  '-class',
+  '-html-safe',
+  '-input-type',
+  '-normalize-class',
+  'concat',
+  'get',
+  'mut',
+  'readonly',
+  'unbound',
+  'debugger',
+  'else',
+  'let',
+  'with',
+  'log',
+  'loc',
+  'hash',
+  'partial',
+  'yield',
+  't',
+  't-for',
+  'transition-to',
+  'get-meta',
+  'get-attr',
+  'index-of',
+  'array',
 
   //ember-moment
-  "moment",
-  "moment-format",
-  "moment-from-now",
-  "moment-from",
-  "moment-to",
-  "moment-to-now",
-  "moment-duration",
-  "moment-calendar",
-  "moment-diff",
+  'moment',
+  'moment-format',
+  'moment-from-now',
+  'moment-from',
+  'moment-to',
+  'moment-to-now',
+  'moment-duration',
+  'moment-calendar',
+  'moment-diff',
 
-  "outlet",
+  'outlet',
 
-  "is-before",
-  "is-after",
-  "is-same",
-  "is-same-or-before",
-  "is-same-or-after",
-  "is-between",
-  "now",
-  "unix",
+  'is-before',
+  'is-after',
+  'is-same',
+  'is-same-or-before',
+  'is-same-or-after',
+  'is-between',
+  'now',
+  'unix',
 
   //cp-validations
-  "v-get",
+  'v-get',
 
   //route-action
-  "route-action",
+  'route-action',
 
   // composable-helpers
-  "map-by",
-  "sort-by",
-  "filter-by",
-  "reject-by",
-  "find-by",
-  "object-at",
-  "hasBlock",
-  "has-block",
-  "has-next",
-  "has-previous",
-  "group-by",
-  "not-eq",
-  "is-array",
-  "is-empty",
-  "is-equal",
+  'map-by',
+  'sort-by',
+  'filter-by',
+  'reject-by',
+  'find-by',
+  'object-at',
+  'hasBlock',
+  'has-block',
+  'has-next',
+  'has-previous',
+  'group-by',
+  'not-eq',
+  'is-array',
+  'is-empty',
+  'is-equal',
 
   // liquid
-  "liquid-unless",
-  "liquid-container",
-  "liquid-outlet",
-  "liquid-versions",
-  "liquid-bind",
-  "liquid-spacer",
-  "liquid-sync",
-  "liquid-measured",
-  "liquid-child",
-  "liquid-if",
+  'liquid-unless',
+  'liquid-container',
+  'liquid-outlet',
+  'liquid-versions',
+  'liquid-bind',
+  'liquid-spacer',
+  'liquid-sync',
+  'liquid-measured',
+  'liquid-child',
+  'liquid-if',
 
   //app-version
-  "app-version"
+  'app-version',
 ];
 
 const isAttribute = key => {
-  return HTML_ATTRIBUTES.includes(key) || key.startsWith("data-");
+  return HTML_ATTRIBUTES.includes(key) || key.startsWith('data-');
 };
 
 const isNestedComponentTagName = tagName => {
-  return tagName && tagName.includes && (tagName.includes("/") || tagName.includes("-"));
+  return tagName && tagName.includes && (tagName.includes('/') || tagName.includes('-'));
 };
 
 /**
@@ -162,15 +162,15 @@ const isNestedComponentTagName = tagName => {
  */
 const capitalizedTagName = tagname => {
   return tagname
-    .split("-")
+    .split('-')
     .map(s => {
       return s[0].toUpperCase() + s.slice(1);
     })
-    .join("");
+    .join('');
 };
 
 const transformTagName = tagName => {
-  if (tagName.includes(".")) {
+  if (tagName.includes('.')) {
     return tagName;
   }
 
@@ -182,15 +182,15 @@ const transformTagName = tagName => {
 };
 
 const transformNestedTagName = tagName => {
-  const paths = tagName.split("/");
-  return paths.map(name => capitalizedTagName(name)).join("::");
+  const paths = tagName.split('/');
+  return paths.map(name => capitalizedTagName(name)).join('::');
 };
 
 const transformNestedSubExpression = subExpression => {
   let positionalArgs = subExpression.params.map(param => {
-    if (param.type === "SubExpression") {
+    if (param.type === 'SubExpression') {
       return transformNestedSubExpression(param);
-    } else if (param.type === "StringLiteral") {
+    } else if (param.type === 'StringLiteral') {
       return `"${param.original}"`;
     } else {
       return param.original;
@@ -200,11 +200,11 @@ const transformNestedSubExpression = subExpression => {
   let namedArgs = [];
   if (subExpression.hash.pairs.length > 0) {
     namedArgs = subExpression.hash.pairs.map(pair => {
-      if (pair.value.type === "SubExpression") {
+      if (pair.value.type === 'SubExpression') {
         let nestedValue = transformNestedSubExpression(pair.value);
         return `${pair.key}=${nestedValue}`;
       } else {
-        if (pair.value.type === "StringLiteral") {
+        if (pair.value.type === 'StringLiteral') {
           return `${pair.key}="${pair.value.original}"`;
         }
         return `${pair.key}=${pair.value.original}`;
@@ -213,13 +213,13 @@ const transformNestedSubExpression = subExpression => {
   }
 
   let args = positionalArgs.concat(namedArgs);
-  return `(${subExpression.path.original} ${args.join(" ")})`;
+  return `(${subExpression.path.original} ${args.join(' ')})`;
 };
 
 const shouldSkipFile = (fileInfo, config) => {
   let source = fileInfo.source;
 
-  if (source.includes("~")) {
+  if (source.includes('~')) {
     //skip files with `~` until https://github.com/ember-codemods/ember-angle-brackets-codemod/issues/46 is resolved
     console.warn(
       `WARNING: ${fileInfo.path} was not converted as it contains a "~" (https://github.com/ember-codemods/ember-angle-brackets-codemod/issues/46)`
@@ -253,8 +253,8 @@ module.exports = function(fileInfo, api, options) {
   }
 
   const ast = glimmer.preprocess(fileInfo.source, {
-    mode: "codemod",
-    parseOptions: { ignoreStandalone: true }
+    mode: 'codemod',
+    parseOptions: { ignoreStandalone: true },
   });
   const b = glimmer.builders;
 
@@ -270,38 +270,38 @@ module.exports = function(fileInfo, api, options) {
         _key = `@${_key}`;
       }
 
-      if (_valueType === "PathExpression") {
+      if (_valueType === 'PathExpression') {
         _value = b.mustache(b.path(a.value.original));
-      } else if (_valueType === "SubExpression") {
+      } else if (_valueType === 'SubExpression') {
         if (a.value.hash.pairs.length > 0) {
           _value = b.mustache(a.value.path.original, a.value.params, a.value.hash);
         } else {
           const params = a.value.params
             .map(p => {
-              if (p.type === "SubExpression") {
+              if (p.type === 'SubExpression') {
                 return transformNestedSubExpression(p);
-              } else if (p.type === "StringLiteral") {
+              } else if (p.type === 'StringLiteral') {
                 return `"${p.original}"`;
-              } else if (p.type === "NullLiteral") {
-                return "null";
-              } else if (p.type === "UndefinedLiteral") {
-                return "undefined";
+              } else if (p.type === 'NullLiteral') {
+                return 'null';
+              } else if (p.type === 'UndefinedLiteral') {
+                return 'undefined';
               } else {
                 return p.original;
               }
             })
-            .join(" ");
+            .join(' ');
 
           _value = b.mustache(b.path(`${a.value.path.original} ${params}`));
         }
-      } else if (_valueType === "BooleanLiteral") {
+      } else if (_valueType === 'BooleanLiteral') {
         _value = b.mustache(b.boolean(a.value.original));
-      } else if (_valueType === "NumberLiteral") {
+      } else if (_valueType === 'NumberLiteral') {
         _value = b.mustache(b.number(a.value.original));
-      } else if (_valueType === "NullLiteral") {
-        _value = b.mustache("null");
-      } else if (_valueType === "UndefinedLiteral") {
-        _value = b.mustache("undefined");
+      } else if (_valueType === 'NullLiteral') {
+        _value = b.mustache('null');
+      } else if (_valueType === 'UndefinedLiteral') {
+        _value = b.mustache('undefined');
       } else {
         _value = b.text(a.value.original || _EMPTY_STRING_);
       }
@@ -313,14 +313,14 @@ module.exports = function(fileInfo, api, options) {
   const isQueryParam = param => {
     return (
       param &&
-      param.type === "SubExpression" &&
+      param.type === 'SubExpression' &&
       param.path &&
-      param.path.original === "query-params"
+      param.path.original === 'query-params'
     );
   };
 
   const transformLinkToTextParam = textParam => {
-    if (textParam.type.includes("Literal")) {
+    if (textParam.type.includes('Literal')) {
       return b.text(textParam.value);
     } else {
       return b.mustache(textParam.original);
@@ -329,9 +329,9 @@ module.exports = function(fileInfo, api, options) {
 
   const transformModelParams = modelParam => {
     let type = modelParam.type;
-    if (type === "StringLiteral") {
+    if (type === 'StringLiteral') {
       return b.text(modelParam.value);
-    } else if (type === "NumberLiteral") {
+    } else if (type === 'NumberLiteral') {
       return b.mustache(b.number(modelParam.original));
     } else {
       return b.mustache(modelParam.original);
@@ -347,11 +347,11 @@ module.exports = function(fileInfo, api, options) {
     let firstParamOutput;
 
     if (isQueryParam(firstParamInput)) {
-      firstParamOutput = b.attr("@query", b.mustache(b.path("hash"), [], firstParamInput.hash));
-    } else if (firstParamInput.type === "PathExpression") {
-      firstParamOutput = b.attr("@route", b.mustache(firstParamInput.original));
+      firstParamOutput = b.attr('@query', b.mustache(b.path('hash'), [], firstParamInput.hash));
+    } else if (firstParamInput.type === 'PathExpression') {
+      firstParamOutput = b.attr('@route', b.mustache(firstParamInput.original));
     } else {
-      firstParamOutput = b.attr("@route", b.text(firstParamInput.value));
+      firstParamOutput = b.attr('@route', b.text(firstParamInput.value));
     }
 
     if (params.length === 1) {
@@ -359,26 +359,28 @@ module.exports = function(fileInfo, api, options) {
     } else if (params.length === 2) {
       // @route and @model param
 
+      // eslint-disable-next-line no-unused-vars
       let [_, secondParamInput] = params;
-      if (secondParamInput.type === "SubExpression") {
-        let _queryParam = b.attr("@query", b.mustache(b.path("hash"), [], secondParamInput.hash));
+      if (secondParamInput.type === 'SubExpression') {
+        let _queryParam = b.attr('@query', b.mustache(b.path('hash'), [], secondParamInput.hash));
         attributes = [firstParamOutput, _queryParam];
       } else {
-        let _modelParam = b.attr("@model", transformModelParams(secondParamInput));
+        let _modelParam = b.attr('@model', transformModelParams(secondParamInput));
         attributes = [firstParamOutput, _modelParam];
       }
     } else if (params.length > 2) {
       // @route and @models params
+      // eslint-disable-next-line no-unused-vars
       let [_, ...models] = params;
       let hasQueryParamHelper = isQueryParam(models[models.length - 1]);
       let _modelsParam;
       let _qpParam;
 
       if (hasQueryParamHelper) {
-        _modelsParam = b.attr("@model", transformModelParams(models[0]));
-        _qpParam = b.attr("@query", b.mustache(b.path("hash"), [], models[1].hash));
+        _modelsParam = b.attr('@model', transformModelParams(models[0]));
+        _qpParam = b.attr('@query', b.mustache(b.path('hash'), [], models[1].hash));
       } else {
-        _modelsParam = b.attr("@models", b.mustache(b.path("array"), models));
+        _modelsParam = b.attr('@models', b.mustache(b.path('array'), models));
       }
       attributes = [firstParamOutput, _modelsParam];
       if (_qpParam) {
@@ -405,11 +407,11 @@ module.exports = function(fileInfo, api, options) {
   };
 
   const getDataAttributesFromParams = params => {
-    return params.filter(param => param.original && `${param.original}`.startsWith("data-"));
+    return params.filter(param => param.original && `${param.original}`.startsWith('data-'));
   };
 
   const getNonDataAttributesFromParams = params => {
-    return params.filter(p => !(p.original && `${p.original}`.startsWith("data-")));
+    return params.filter(p => !(p.original && `${p.original}`.startsWith('data-')));
   };
 
   const shouldIgnoreMustacheStatement = name => {
@@ -420,9 +422,9 @@ module.exports = function(fileInfo, api, options) {
     if (node.params.length > 0) {
       let firstParamType = node.params[0].type;
 
-      if (["StringLiteral", "NumberLiteral", "SubExpression"].includes(firstParamType)) {
+      if (['StringLiteral', 'NumberLiteral', 'SubExpression'].includes(firstParamType)) {
         return true;
-      } else if (firstParamType === "PathExpression") {
+      } else if (firstParamType === 'PathExpression') {
         if (!isAttribute(node.params[0].original)) {
           return true;
         }
@@ -445,8 +447,8 @@ module.exports = function(fileInfo, api, options) {
     let children = node.program ? node.program.body : undefined;
     let blockParams = node.program ? node.program.blockParams : undefined;
 
-    if (tagName === "link-to") {
-      if (node.type === "MustacheStatement") {
+    if (tagName === 'link-to') {
+      if (node.type === 'MustacheStatement') {
         let params = node.params;
         let textParam = params.shift(); //the first param becomes the block content
 
@@ -472,7 +474,7 @@ module.exports = function(fileInfo, api, options) {
     return b.element(newTagName, {
       attrs: attributes,
       children,
-      blockParams
+      blockParams,
     });
   };
 
@@ -480,7 +482,7 @@ module.exports = function(fileInfo, api, options) {
     MustacheStatement(node) {
       // Don't change attribute statements
       const isValidMustache =
-        node.loc.source !== "(synthetic)" && !shouldIgnoreMustacheStatement(node.path.original);
+        node.loc.source !== '(synthetic)' && !shouldIgnoreMustacheStatement(node.path.original);
       const tagName = node.path.original;
       const isNestedComponent = isNestedComponentTagName(tagName);
 
@@ -500,19 +502,19 @@ module.exports = function(fileInfo, api, options) {
 
     ElementNode(node) {
       node.attributes.forEach(a => {
-        if (a.value && a.value.chars === "") {
+        if (a.value && a.value.chars === '') {
           a.value = b.text(_EMPTY_STRING_);
         }
       });
-    }
+    },
   });
 
-  let attrEqualEmptyString = new RegExp(_EMPTY_STRING_, "gi");
+  let attrEqualEmptyString = new RegExp(_EMPTY_STRING_, 'gi');
   let dataEqualsNoValue = /(data-\S+)=""/gim;
 
   // Haxx out valueless data-* and args with the empty string
 
-  let uglySource = glimmer.print(ast).replace(attrEqualEmptyString, "");
-  let dataOk = uglySource.replace(dataEqualsNoValue, "$1");
-  return prettier.format(dataOk, { parser: "glimmer" });
+  let uglySource = glimmer.print(ast).replace(attrEqualEmptyString, '');
+  let dataOk = uglySource.replace(dataEqualsNoValue, '$1');
+  return prettier.format(dataOk, { parser: 'glimmer' });
 };
