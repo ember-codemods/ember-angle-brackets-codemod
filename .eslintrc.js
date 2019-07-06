3ff5f87cb85d3a69299335c5f03d31972d83d459
Ryan Mark <rgmark@gmail.com>
2019-07-05 20:34:48 -0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	root: true,
+	parserOptions: {
+	  ecmaVersion: 2017
+	},
+	env: {
+	  node: true,
+	  es6: true
+	},
+	extends: ["eslint:recommended", "prettier"],
+	plugins: ["prettier"],
+	rules: {
+	  "prettier/prettier": "error"
+	},
+	overrides: [
+	  {
+		files: ['**/*.test.js'],
+		env: {
+		  jest: true
+		}
+	  }
+	]
+  };
