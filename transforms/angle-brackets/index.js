f121811456f7bb97f2f2013c9d1b10f51682cae6
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-30 08:40:07 +0200

    Remove unnecessary `Object.assign()` call
    There is no need to clone `file` for this

--- transforms/angle-brackets/index.js
+++ transforms/angle-brackets/index.js
@@ -11,7 +11,7 @@ module.exports = function(file, api) {
     return;
   }
   try {
-    src = fix(Object.assign({}, file, { source: src }), api, options);
+    src = fix(file, api, options);
   } catch (e) {
     throw new Error(
       `Transformation errored on file ${file.path}. Reason ${e}. Please report this in https://github.com/ember-codemods/ember-angle-brackets-codemod/issues\n\nStack trace:\n${e.stack}\n\nSource:\n${src}`
