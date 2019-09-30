b91b5c9c3ded8be0c75875dbdb5ae9a31d5f0931
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-30 14:02:38 +0200

    CI: Run ESLint (#139)
    * bin/cli: Fix linting issues
    
    * package.json: Add `lint` script
    
    * CI: Run ESLint

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,6 @@
 
 require('codemod-cli').runTransform(
   __dirname,
-  process.argv[2]       /* transform name */,
+  process.argv[2] /* transform name */,
   process.argv.slice(3) /* paths or globs */
-)
+);
