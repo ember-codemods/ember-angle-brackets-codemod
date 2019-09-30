34ba01ad1075e476ad751920e6772a9494feecaa
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-30 07:59:39 +0200

    Remove unnecessary `error` variable

--- transforms/angle-brackets/index.js
+++ transforms/angle-brackets/index.js
@@ -5,7 +5,6 @@ const { getOptions } = require('codemod-cli');
 module.exports = function(file, api) {
   let src = file.source;
   let options = getOptions();
-  let error;
 
   let fix = require('./transforms/angle-brackets-syntax');
   if (typeof src === 'undefined') {
@@ -14,13 +13,10 @@ module.exports = function(file, api) {
   try {
     src = fix(Object.assign({}, file, { source: src }), api, options);
   } catch (e) {
-    error = new Error(
+    throw new Error(
       `Transformation errored on file ${file.path}. Reason ${e}. Please report this in https://github.com/ember-codemods/ember-angle-brackets-codemod/issues\n\nStack trace:\n${e.stack}\n\nSource:\n${src}`
     );
   }
 
-  if (error) {
-    throw error;
-  }
   return src;
 };
