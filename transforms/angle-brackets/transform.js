72c17f3a39966c89712f93fdbb21511207380744
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-10-01 10:45:06 +0200

    Extract `IGNORE_MUSTACHE_STATEMENTS` into a dedicated file

--- transforms/angle-brackets/transform.js
+++ transforms/angle-brackets/transform.js
@@ -1,5 +1,7 @@
 const recast = require('ember-template-recast');
 
+const IGNORE_MUSTACHE_STATEMENTS = require('./known-helpers');
+
 const _EMPTY_STRING_ = `ANGLE_BRACKET_EMPTY_${Date.now()}`;
 
 /**
@@ -8,137 +10,6 @@ const _EMPTY_STRING_ = `ANGLE_BRACKET_EMPTY_${Date.now()}`;
 const HTML_ATTRIBUTES = ['class', 'placeholder', 'required'];
 
 const BUILT_IN_COMPONENTS = ['link-to', 'input', 'textarea'];
-
-/**
- * Ignore the following list of MustacheStatements from transform
- * Politely lifted from https://github.com/lifeart/ember-ast-hot-load/blob/master/lib/ast-transform.js#L26
- */
-const IGNORE_MUSTACHE_STATEMENTS = [
-  // Ember.js
-  'action',
-  'array',
-  'component',
-  'concat',
-  'debugger',
-  'each',
-  'each-in',
-  'else',
-  'get',
-  'hash',
-  'if',
-  'if-unless',
-  'in-element',
-  '-in-element',
-  'let',
-  'loc',
-  'log',
-  'mut',
-  'outlet',
-  'partial',
-  'query-params',
-  'readonly',
-  'unbound',
-  'unless',
-  'with',
-  'yield',
-
-  // Glimmer VM
-  'identity', // glimmer blocks
-  'render-inverse', // glimmer blocks
-  '-get-dynamic-var', // glimmer internal helper
-
-  // ember-route-helpers
-  'transition-to',
-  'replace-with',
-  'transition-to-external',
-  'replace-with-external',
-
-  // ember-intl
-  'format-date',
-  'format-message',
-  'format-relative',
-  'format-time',
-  'format-money',
-  'format-number',
-  't',
-
-  // ember-moment
-  'is-after',
-  'is-before',
-  'is-between',
-  'is-same',
-  'is-same-or-after',
-  'is-same-or-before',
-  'moment',
-  'moment-calendar',
-  'moment-diff',
-  'moment-duration',
-  'moment-format',
-  'moment-from',
-  'moment-from-now',
-  'moment-to',
-  'moment-to-now',
-  'now',
-  'unix',
-
-  // ember-cp-validations
-  'v-get',
-
-  // ember-route-action-helper
-  'route-action',
-
-  // ember-composable-helpers
-  'map-by',
-  'sort-by',
-  'filter-by',
-  'reject-by',
-  'find-by',
-  'object-at',
-  'has-block',
-  'has-next',
-  'has-previous',
-  'group-by',
-  'not-eq',
-  'is-array',
-  'is-empty',
-  'is-equal',
-
-  // liquid-fire
-  'liquid-unless',
-  'liquid-container',
-  'liquid-outlet',
-  'liquid-versions',
-  'liquid-bind',
-  'liquid-spacer',
-  'liquid-sync',
-  'liquid-measured',
-  'liquid-child',
-  'liquid-if',
-
-  // ember-animated
-  'animated-beacon',
-  'animated-each',
-  'animated-if',
-  'animated-orphans',
-  'animated-value',
-
-  // ember-app-version
-  'app-version',
-
-  // ember-font-awesome
-  'fa-icon',
-  'fa-list',
-  'fa-stack',
-
-  // ember-svg-jar
-  'svg-jar',
-
-  // ember-concurrency
-  'perform',
-
-  // ember-maybe-in-element
-  'maybe-in-element',
-];
 
 function isAttribute(key) {
   return HTML_ATTRIBUTES.includes(key) || key.startsWith('data-');
